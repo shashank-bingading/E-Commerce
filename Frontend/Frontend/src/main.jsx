a9e5a3525ac1901103c340c2407a7553e7770a5e
Shashank Singh <shashanksinghwork88@gmail.com>
2026-04-04 23:26:35 +0530

    Added basic frontend structure and files

--- Frontend/Frontend/src/main.jsx
+++ Frontend/Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import store from "./pages/ProfileScreen.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      // The App component, wrapped with the Provider to give it access to the Redux store
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
---
+++ Frontend/Frontend/src/pages/ProfileScreen.jsx
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {
+        // Adding reducers here in later commits
+    },
+});
+
+export default store;
